2eaa155bc1728208a1ae9f4595f1e1337ec6c8e3
SAF <sfamilleza@gmail.com>
2025-08-01 21:42:25 +0800

    Update button.tsx

--- src/app/demo/[name]/ui/button.tsx
+++ src/app/demo/[name]/ui/button.tsx
@@ -1,10 +1,30 @@
 import { Button } from "@/components/ui/button";
+import { ChevronRightIcon, Loader2Icon, GitBranch } from "lucide-react";
 
 export const button = {
   name: "button",
   components: {
-    Primary: <Button variant="default">Primary</Button>,
+    Default: <Button>Button</Button>,
     Secondary: <Button variant="secondary">Secondary</Button>,
+    Destructive: <Button variant="destructive">Destructive</Button>,
     Outline: <Button variant="outline">Outline</Button>,
+    Ghost: <Button variant="ghost">Ghost</Button>,
+    Link: <Button variant="link">Link</Button>,
+    Icon: (
+      <Button variant="secondary" size="icon" className="size-8">
+        <ChevronRightIcon />
+      </Button>
+    ),
+    WithIcon: (
+      <Button variant="outline" size="sm">
+        <GitBranch /> New Branch
+      </Button>
+    ),
+    Loading: (
+      <Button size="sm" disabled>
+        <Loader2Icon className="animate-spin" />
+        Please wait
+      </Button>
+    ),
   },
 };
